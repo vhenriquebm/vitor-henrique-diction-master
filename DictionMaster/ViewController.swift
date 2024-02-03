3ad71c9daf066688aef01829f7ce669a488e91f2
vhenriquebm <vhenriquebm@gmail.com>
2024-02-03 11:13:12 -0300

    Initial Commit

---
+++ DictionMaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DictionMaster
+//
+//  Created by Vitor Henrique Barreiro Marinho on 03/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
